8cccec4e3a731af300a434b6df4a72fadcc7233f
walfly <walker.d.flynn@gmail.com>
2014-02-09 20:36:23 -0500

    initial set up

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = angular.module('app', []).controller('Ctrl', function ($scope) {
+  $scope.repeatArr = [
+    {'id' : 1, 'content': 'one'},
+    {'id' : 2, 'content': 'two'},
+    {'id' : 3, 'content': 'three'},
+    {'id' : 4, 'content': 'four'},
+    {'id' : 5, 'content': 'five'},
+    {'id' : 6, 'content': 'six'},
+    {'id' : 7, 'content': 'seven'},
+    {'id' : 8, 'content': 'eight'}
+  ];
+});
